78f0550639baf10e4f83043c1dead7224901ce4b
Gavin Peacock <gpeacock@adobe.com>
2024-11-18 10:12:33 -0800

    feat: Adds Reader.to_folder support

--- include/c2pa.h
+++ include/c2pa.h
@@ -319,6 +319,19 @@ int c2pa_reader_resource_to_stream(struct C2paReader *reader_ptr,
                                    struct CStream *stream);
 
 /**
+ * Writes all the resources in a Reader to a folder.
+ *
+ * # Parameters
+ * * reader_ptr: pointer to a Reader.
+ * * folder_path: pointer to a C string with the path to the folder.
+ *
+ * # Errors
+ * Returns -1 if there were errors, otherwise returns 0.
+ * The error string can be retrieved by calling c2pa_error.
+ */
+IMPORT extern int c2pa_reader_to_folder(struct C2paReader *reader_ptr, const char *folder_path);
+
+/**
  * Creates a C2paBuilder from a JSON manifest definition string.
  *
  * # Errors
@@ -662,7 +675,7 @@ struct CStream *c2pa_create_stream(struct StreamContext *context,
 IMPORT extern void c2pa_release_stream(struct CStream *stream);
 
 #ifdef __cplusplus
-} // extern "C"
-#endif // __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
 
-#endif /* c2pa_bindings_h */
+#endif  /* c2pa_bindings_h */
