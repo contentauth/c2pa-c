68c22b2c825a63f939e9b6be1a8f9e4a3ccf9e42
Tania Mathern <mathern@adobe.com>
2025-05-13 09:54:10 -0700

    fix: Add a test for v2 claims signer

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -29,37 +29,37 @@ class TestC2paSdk(unittest.TestCase):
         self.assertIn("0.8.0", sdk_version())
 
 
-# class TestReader(unittest.TestCase):
-#     def test_stream_read(self):
-#         with open(testPath, "rb") as file:
-#             with Reader("image/jpeg", file) as reader:
-#                 json = reader.json()
-#                 self.assertIn("C.jpg", json)
+class TestReader(unittest.TestCase):
+    def test_stream_read(self):
+        with open(testPath, "rb") as file:
+            with Reader("image/jpeg", file) as reader:
+                json = reader.json()
+                self.assertIn("C.jpg", json)
 
-#     def test_stream_read_and_parse(self):
-#         with open(testPath, "rb") as file:
-#             with Reader("image/jpeg", file) as reader:
-#                 manifest_store = json.loads(reader.json())
-#                 title = manifest_store["manifests"][manifest_store["active_manifest"]]["title"]
-#                 self.assertEqual(title, "C.jpg")
+    def test_stream_read_and_parse(self):
+        with open(testPath, "rb") as file:
+            with Reader("image/jpeg", file) as reader:
+                manifest_store = json.loads(reader.json())
+                title = manifest_store["manifests"][manifest_store["active_manifest"]]["title"]
+                self.assertEqual(title, "C.jpg")
 
-#     def test_json_decode_err(self):
-#         with self.assertRaises(Error.Io):
-#             with Reader("image/jpeg", "foo") as reader:
-#                 manifest_store = reader.json()
+    def test_json_decode_err(self):
+        with self.assertRaises(Error.Io):
+            with Reader("image/jpeg", "foo") as reader:
+                manifest_store = reader.json()
 
-#     def test_reader_bad_format(self):
-#         with self.assertRaises(Error.NotSupported):
-#             with open(testPath, "rb") as file:
-#                 with Reader("badFormat", file) as reader:
-#                     pass
+    def test_reader_bad_format(self):
+        with self.assertRaises(Error.NotSupported):
+            with open(testPath, "rb") as file:
+                with Reader("badFormat", file) as reader:
+                    pass
 
-#     def test_settings_trust(self):
-#         #load_settings_file("tests/fixtures/settings.toml")
-#         with open(testPath, "rb") as file:
-#             with Reader("image/jpeg", file) as reader:
-#                 json = reader.json()
-#                 self.assertIn("C.jpg", json)
+    def test_settings_trust(self):
+        #load_settings_file("tests/fixtures/settings.toml")
+        with open(testPath, "rb") as file:
+            with Reader("image/jpeg", file) as reader:
+                json = reader.json()
+                self.assertIn("C.jpg", json)
 
 class TestBuilder(unittest.TestCase):
     # Define a manifest as a dictionary
@@ -69,6 +69,32 @@ class TestBuilder(unittest.TestCase):
             "name": "python_test",
             "version": "0.0.1",
         }],
+        "format": "image/jpeg",
+        "title": "Python Test Image",
+        "ingredients": [],
+        "assertions": [
+            {   'label': 'stds.schema-org.CreativeWork',
+                'data': {
+                    '@context': 'http://schema.org/',
+                    '@type': 'CreativeWork',
+                    'author': [
+                        {   '@type': 'Person',
+                            'name': 'Gavin Peacock'
+                        }
+                    ]
+                },
+                'kind': 'Json'
+            }
+        ]
+    }
+
+    manifestDefinitionV2 = {
+        "claim_generator": "python_test",
+        "claim_generator_info": [{
+            "name": "python_test",
+            "version": "0.0.1",
+        }],
+        "claim_version": 2,
         "format": "image/jpeg",
         "title": "Python Test Image",
         "ingredients": [],
@@ -116,33 +142,33 @@ class TestBuilder(unittest.TestCase):
         self.assertIn("Python Test", json_data)
         self.assertNotIn("validation_status", json_data)
 
-    # def test_streams_sign(self):
-    #     with open(testPath, "rb") as file, \
-    #          io.BytesIO(bytearray()) as output, \
-    #          Builder(TestBuilder.manifestDefinition) as builder:
-    #         builder.sign(TestBuilder.signer, "image/jpeg", file, output)
-    #         output.seek(0)
-    #         with Reader("image/jpeg", output) as reader:
-    #             self._read_manifest(reader)
-
-    # def test_streams_sign_with_ingredient(self):
-    #     with open(testPath, "rb") as file, \
-    #          io.BytesIO(bytearray()) as output, \
-    #          Builder(TestBuilder.manifestDefinition) as builder:
-
-    #         ingredient_json = '{"title": "test-ingredient"}'
-    #         with open(os.path.join(PROJECT_PATH, "tests", "fixtures", "A.png"), 'rb') as f:
-    #             builder.add_ingredient(ingredient_json, "image/png", f)
-
-    #             builder.sign(TestBuilder.signer, "image/jpeg", file, output)
-    #             output.seek(0)
-    #             with Reader("image/jpeg", output) as reader:
-    #                 self._read_manifest(reader)
-
-    def test_streams_sign_with_ingredient_with_manifest(self):
+    def test_streams_sign(self):
         with open(testPath, "rb") as file, \
              io.BytesIO(bytearray()) as output, \
              Builder(TestBuilder.manifestDefinition) as builder:
+            builder.sign(TestBuilder.signer, "image/jpeg", file, output)
+            output.seek(0)
+            with Reader("image/jpeg", output) as reader:
+                self._read_manifest(reader)
+
+    def test_streams_sign_with_ingredient(self):
+        with open(testPath, "rb") as file, \
+             io.BytesIO(bytearray()) as output, \
+             Builder(TestBuilder.manifestDefinition) as builder:
+
+            ingredient_json = '{"title": "test-ingredient"}'
+            with open(os.path.join(PROJECT_PATH, "tests", "fixtures", "A.png"), 'rb') as f:
+                builder.add_ingredient(ingredient_json, "image/png", f)
+
+                builder.sign(TestBuilder.signer, "image/jpeg", file, output)
+                output.seek(0)
+                with Reader("image/jpeg", output) as reader:
+                    self._read_manifest(reader)
+
+    def test_streams_sign_with_ingredient_with_manifest_v2(self):
+        with open(testPath, "rb") as file, \
+             io.BytesIO(bytearray()) as output, \
+             Builder(TestBuilder.manifestDefinitionV2) as builder:
 
             ingredient_json = '{"title": "test-ingredient"}'
             with open(os.path.join(PROJECT_PATH, "tests", "fixtures", "A-signed.png"), 'rb') as f:
@@ -155,28 +181,28 @@ class TestBuilder(unittest.TestCase):
                 with Reader("image/jpeg", output) as reader:
                     self._read_manifest(reader)
 
-    # def test_archive_sign(self):
-    #     with open(testPath, "rb") as file, \
-    #          io.BytesIO(bytearray()) as archive:
-    #         with Builder(TestBuilder.manifestDefinition) as builder:
-    #             builder.to_archive(archive)
-    #             archive.seek(0)
-    #             with Builder.from_archive(archive) as builder2, \
-    #                  io.BytesIO(bytearray()) as output:
-    #                 builder2.sign(TestBuilder.signer, "image/jpeg", file, output)
-    #                 output.seek(0)
-    #                 with Reader("image/jpeg", output) as reader:
-    #                     self._read_manifest(reader)
+    def test_archive_sign(self):
+        with open(testPath, "rb") as file, \
+             io.BytesIO(bytearray()) as archive:
+            with Builder(TestBuilder.manifestDefinition) as builder:
+                builder.to_archive(archive)
+                archive.seek(0)
+                with Builder.from_archive(archive) as builder2, \
+                     io.BytesIO(bytearray()) as output:
+                    builder2.sign(TestBuilder.signer, "image/jpeg", file, output)
+                    output.seek(0)
+                    with Reader("image/jpeg", output) as reader:
+                        self._read_manifest(reader)
 
-    # def test_remote_sign(self):
-    #     with open(testPath, "rb") as file, \
-    #          io.BytesIO(bytearray()) as output, \
-    #          Builder(TestBuilder.manifestDefinition) as builder:
-    #         builder.set_no_embed()
-    #         manifest_data = builder.sign(TestBuilder.signer, "image/jpeg", file, output)
-    #         output.seek(0)
-    #         with Reader("image/jpeg", output, manifest_data) as reader:
-    #             self._read_manifest(reader)
+    def test_remote_sign(self):
+        with open(testPath, "rb") as file, \
+             io.BytesIO(bytearray()) as output, \
+             Builder(TestBuilder.manifestDefinition) as builder:
+            builder.set_no_embed()
+            manifest_data = builder.sign(TestBuilder.signer, "image/jpeg", file, output)
+            output.seek(0)
+            with Reader("image/jpeg", output, manifest_data) as reader:
+                self._read_manifest(reader)
 
 if __name__ == '__main__':
     unittest.main()
