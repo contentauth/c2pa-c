19acabfaa76119baf04bb6985700a0b8fa10b2f6
Tania Mathern <mathern@adobe.com>
2025-09-04 11:54:25 -0700

    fix: DngStream

--- build_debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mathern/Desktop/git/@CAI/c2pa-c
-# Build directory: /Users/mathern/Desktop/git/@CAI/c2pa-c/build_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
-subdirs("examples")
