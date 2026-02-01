9775be5466a1e857de63511e2fbf7f58694ff1dd
tmathern <60901087+tmathern@users.noreply.github.com>
2026-02-01 13:46:18 -0800

    Change reserve_size method to non-const

--- src/c2pa.cpp
+++ src/c2pa.cpp
@@ -644,7 +644,7 @@ namespace c2pa
     }
 
     /// @brief  Get the size to reserve for a signature for this signer.
-    uintptr_t Signer::reserve_size() const
+    uintptr_t Signer::reserve_size()
     {
         return c2pa_signer_reserve_size(signer);
     }
