f4f9928048e1c5c75cbfd5145a40eb5b0639e08f
Tania Mathern <mathern@adobe.com>
2026-02-05 14:16:45 -0800

    fix: Streams refacotr

--- tests/builder.test.cpp
+++ tests/builder.test.cpp
@@ -1225,7 +1225,7 @@ TEST(Builder, SignStreamCloudUrl)
     catch (c2pa::C2paException const &e)
     {
         std::string error_message = e.what();
-        if (error_message.rfind("Other: could not fetch the remote manifest", 0) == 0)
+        if (error_message.rfind("Remote:", 0) == 0)
         {
             SUCCEED();
         }
